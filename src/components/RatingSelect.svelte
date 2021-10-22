e69d6de6fe785ac00c5bfef5a83b275646e083e1
HOUSNI Mohamed <mohamed.housni@um5s.net.ma>
2021-10-22 17:35:10 +0100

    added rating select, add/delete feedback plus transitio effetcts

--- src/components/RatingSelect.svelte
+++ src/components/RatingSelect.svelte
@@ -1,10 +1,16 @@
 <script>
+import {createEventDispatcher} from 'svelte'
+const dispatch = createEventDispatcher()
 let ratings =  [1,2,3,4,5,6,7,8,9,10]
+
+const onChange = (e) => {
+  dispatch('rating-select', e.currentTarget.value)
+}
 </script>
 
 <div class="radio-toolbar">
     {#each ratings as rating (rating)}
-      <input type="radio" id="num{rating}" name="rating" value="{rating}" checked>
+      <input type="radio" id="num{rating}" name="rating" value="{rating}" on:change={onChange}>
       <label for="num{rating}">{rating}</label>
     {/each}
 </div>
@@ -30,7 +36,7 @@ let ratings =  [1,2,3,4,5,6,7,8,9,10]
     padding:10px;
     font-family: sans-serif, Arial;
     font-size: 20px;
-    border: 2px solid #444;
+    border: 2px solid #fff;
     border-radius: 50%;
     text-align: center;
     margin: 5px
@@ -41,12 +47,13 @@ let ratings =  [1,2,3,4,5,6,7,8,9,10]
 }
 
 .radio-toolbar input[type="radio"]:focus + label {
-    border: 2px dashed #444;
+    border: 2px solid #444;
 }
 
 .radio-toolbar input[type="radio"]:checked + label {
-    background-color: #bfb;
-    border-color: #4c4;
+    background-color: #1E1D37;
+    color: #fff;
+    font-size: 20px;
 }
 
 
